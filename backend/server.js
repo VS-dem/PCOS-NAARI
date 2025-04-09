0919c3ea8c5564cb574458aec128ebb434a8f0f7
VRUSHABH SOMANI <vssomani2002@gmail.com>
2025-04-09 21:59:52 +0530

    Server updated

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoute from "./routes/auth.route.js";
+import periodRoute from "./routes/period.route.js";
+import symptomRoute from "./routes/symptoms.route.js";
+import remedyRoute from "./routes/remedy.route.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +16,9 @@ app.use(express.json()); //allows us to parse incoming requests : req.body
 app.use(cookieParser()); //alows us to parse cookie's : req.cookies 
 
 app.use("/api/auth",authRoute);
+app.use("/api/periods",periodRoute);
+app.use("/api/symtpoms",symptomRoute);
+app.use("/api/remedies",remedyRoute);
 
 app.listen(port, () => {
     connectDB();
