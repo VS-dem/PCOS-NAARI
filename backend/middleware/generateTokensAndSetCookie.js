81f56ec1fbf5a140e873e4fb4dc81bc593b00d26
VRUSHABH SOMANI <vssomani2002@gmail.com>
2025-04-09 22:01:00 +0530

    Updated to generate token and set cookies on backend

--- backend/middleware/generateTokensAndSetCookie.js
+++ backend/middleware/generateTokensAndSetCookie.js
@@ -1,16 +1,24 @@
 import jwt from "jsonwebtoken";
 
-export const generateTokenAndSetCookie = (res, userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
-        expiresIn: "7d", // token exires in 7 days
-    })
+export const generateTokenAndSetCookie = (res, user) => {
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d", // Token expires in 7 days
+    }
+  );
 
-    res.cookie("token", token,{
-        httpOnly:true,
-        secure: process.env.NODE_ENV ==="production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
 
-    return token;
-} 
+  return token;
+};
